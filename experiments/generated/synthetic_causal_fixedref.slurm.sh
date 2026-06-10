c2ddc2416f88c51c8a9be8bace1c209c6cf7416d
Natalia Glazman <glazmann@yahoo.co.uk>
2026-06-10 10:49:40 +0100

    change scripts

--- experiments/generated/synthetic_causal_fixedref.slurm.sh
+++ experiments/generated/synthetic_causal_fixedref.slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # Auto-generated from: experiments/synthetic_causal_fixedref.yaml
-# Generated at: 2026-06-10T09:34:12Z
-# Git SHA: 70fefc5
+# Generated at: 2026-06-10T09:49:36Z
+# Git SHA: 4c14ab1
 # Re-generate with: python scripts/launch.py --generate --cluster slurm
 #SBATCH --job-name=synthetic-causal-random-fixedref
 #SBATCH --output=/scratch/users/%u/%j.out
@@ -70,7 +70,6 @@ conda activate "${CONDA_ENV_NAME}"
     --mask-mode fixed \
     --masks-dir /scratch/users/k24058220/ADNI_stripped_masks \
     --moco-queue-size 0 \
-    --nb-levels 1 \
     --pass-full-to-next-level \
     --patch-grid 4 4 4 \
     --recon-loss-start-step 0 \
@@ -100,6 +99,6 @@ conda activate "${CONDA_ENV_NAME}"
     --vqvae-embed-dim 48 \
     --vqvae-hidden-channels 48 \
     --vqvae-nb-entries 256 \
-    --vqvae-nb-levels 3 \
+    --vqvae-nb-levels 1 \
     --vqvae-scaling-rates 4 \
     --workers 8
