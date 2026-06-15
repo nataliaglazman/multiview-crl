453bda61a55d4460e367e2b987c1c8c5ae1daeb0
Natalia Glazman <glazmann@yahoo.co.uk>
2026-06-15 17:24:19 +0100

    change scripts

--- experiments/generated/synthetic_causal.slurm.sh
+++ experiments/generated/synthetic_causal.slurm.sh
@@ -6,13 +6,13 @@
 #SBATCH --job-name=synthetic-causal-random-9-3-2-separate-style-codebooks
 #SBATCH --output=/scratch/users/%u/%j.out
 #SBATCH --error=synthetic-causal-random-9-3-2-separate-style-codebooks-%j.err
-#SBATCH --partition=biomed_a100_gpu
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=8
 #SBATCH --time=48:00:00
-#SBATCH --constraint=a100_80g
+#SBATCH --constraint=a100_40g
 
 # -- Software & Environment Setup --
 module load anaconda3/2022.10-gcc-13.2.0
