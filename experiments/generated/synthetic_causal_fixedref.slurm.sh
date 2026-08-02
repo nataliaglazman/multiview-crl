d799dd40ac76ed0fcb078d9fa1257881c1f9dac6
Natalia Glazman <glazmann@yahoo.co.uk>
2026-08-02 19:52:48 +0100

    change scripts

--- experiments/generated/synthetic_causal_fixedref.slurm.sh
+++ experiments/generated/synthetic_causal_fixedref.slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # Auto-generated from: experiments/synthetic_causal_fixedref.yaml
-# Generated at: 2026-07-31T14:12:00Z
-# Git SHA: 5799bfa
+# Generated at: 2026-08-02T18:52:40Z
+# Git SHA: 7364f3a
 # Re-generate with: python scripts/launch.py --generate --cluster slurm
 #SBATCH --job-name=synthetic-causal-random-fixedref-new
 #SBATCH --output=/scratch/users/%u/%j.out
